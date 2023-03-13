d2c65ad741cf511eff27c0ee7c170453761479e1
Iago Alves <iago@window.finance>
2023-03-13 20:24:47 -0300

    add external display functionality
    Co-authored-by: gmega <giuliano.mega@gmail.com>
    Co-authored-by: iagoLD <iago@window.finance>

--- implementations/string_calculator/string_calculator.py
+++ implementations/string_calculator/string_calculator.py
@@ -1,26 +1,47 @@
 import re
+from abc import ABC, abstractmethod
 
 
-def addition(input_string: str) -> int:
-    if input_string == '':
-        return 0
-    else:
-        numbers = decompose_numbers(input_string)
-        result = 0
-        for n in numbers:
-            number = int(n)
-            if number < 0:
-                raise InvalidInputException
-            result += number
-    return result
+class Display(ABC):
+    @abstractmethod
+    def show(self, string: str):
+        ...
 
 
-def decompose_numbers(input_string: str):
-    if input_string.startswith('//'):
-        delimiter = input_string.split('\n')[0].replace('//', '')
-        return input_string.split('\n')[1].split(delimiter)
-    else:
-        return re.split('[\n|,]', input_string)
+class NullDisplay(Display):
+    def show(self, string: str):
+        pass
+
+
+class StringCalc:
+
+    def __init__(self, display: Display = NullDisplay()):
+        self._display = display
+
+    def add(self, input_string: str) -> int:
+        if input_string == '':
+            return 0
+        else:
+            numbers = self._decompose_numbers(input_string)
+            result = 0
+            for n in numbers:
+                number = int(n)
+                if number < 0:
+                    raise InvalidInputException
+                result += number
+
+            self._display.show(f"{' + '.join(numbers)} = {result}")
+
+        return result
+
+    def _decompose_numbers(self, input_string: str):
+
+        match = re.match(r'^//(?P<delimiter>.)\n(?P<number_sequence>.*)$', input_string)
+
+        if match is not None:
+            return match['number_sequence'].split(match.groupdict()['delimiter'])
+        else:
+            return re.split(r',|\n', input_string)
 
 
 class InvalidInputException(Exception):
--- implementations/string_calculator/test_string_calculator.py
+++ implementations/string_calculator/test_string_calculator.py
@@ -1,28 +1,38 @@
+from unittest.mock import MagicMock
+
 import pytest
 
-from string_calculator.string_calculator import addition, InvalidInputException
+from string_calculator.string_calculator import StringCalc, InvalidInputException
 
 
 def test_empty_string_returns_zero():
-    assert addition('') == 0
+    assert StringCalc().add('') == 0
 
 
 def test_one_number_addition():
-    assert addition('1') == 1
+    assert StringCalc().add('1') == 1
 
 
 def test_two_numbers_addition():
-    assert addition('1,2') == 3
+    assert StringCalc().add('1,2') == 3
 
 
 def test_more_than_two_numbers_addition():
-    assert addition('1\n2,3') == 6
+    assert StringCalc().add('1\n2,3') == 6
 
 
 def test_user_defined_delimiter():
-    assert addition('//;\n1;2') == 3
+    assert StringCalc().add('//;\n1;2') == 3
 
 
 def test_thrown_exception():
     with pytest.raises(InvalidInputException):
-        addition('-1,2')
+        StringCalc().add('-1,2')
+
+
+def test_should_display_calculation():
+    display = MagicMock()
+
+    StringCalc(display).add('1,2,3')
+
+    display.show.assert_called_with('1 + 2 + 3 = 6')
